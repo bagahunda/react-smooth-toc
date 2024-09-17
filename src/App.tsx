0392efae338a2e0873d2c3b401a692c877ed0548
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2024-09-17 16:53:08 +0300

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import '@/App.css'
+import { Box, HStack } from '@kuma-ui/core'
 import { Container } from '@/atoms/container'
 import { MainContent } from './components/main-content'
 import { MarkdownView } from './components/markdown-view'
@@ -9,7 +10,26 @@ function App() {
     <Container>
       <MainContent>
         <PageTitle />
-        <MarkdownView />
+        <HStack gap={[0, 0, 0, '1em', '2em']}>
+          <Box flexGrow={1} minWidth={0}>
+            <MarkdownView />
+          </Box>
+          <Box
+            display={['none', 'none', 'block', 'block']}
+            flexGrow={0}
+            flexShrink={0}
+            flexBasis={['0', '0', '200px', '200px', '300px']}
+          >
+            <Box
+              position={'sticky'}
+              top={16}
+              maxHeight={'calc(100vh - 70px)'}
+              overflowY={'auto'}
+            >
+              Outline goes here
+            </Box>
+          </Box>
+        </HStack>
       </MainContent>
     </Container>
   )
