985017f38bc89f8bc4b8f65c0b084a27eef8a375
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2024-09-17 10:40:52 +0300

    linters settigs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { create } from 'zustand'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type CounterState = {
+  count: number
+  increment: () => void
+}
 
+const useCounterStore = create<CounterState>(set => {
+  return {
+    count: 0,
+    increment: () => set(state => ({ count: state.count + 1 }))
+  }
+})
+
+function App() {
+  const { count, increment } = useCounterStore()
   return (
     <>
       <div>
@@ -18,9 +29,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={increment}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
