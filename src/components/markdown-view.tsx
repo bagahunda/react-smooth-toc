637c70b22e31aeed1261c500fb55bf087aa6e21e
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2024-09-17 14:19:24 +0300

    style markdown

--- src/components/markdown-view.tsx
+++ src/components/markdown-view.tsx
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react'
 import { Box } from '@kuma-ui/core'
 import markdownContent from '@/example.md?raw'
 import { useContentStore } from '@/stores/content'
+import './markdown-view.css'
 
 export const MarkdownView: React.FC = () => {
   const { dom, render } = useContentStore()
   useEffect(() => {
     render(markdownContent)
   }, [render])
-  return (
-    <Box className="markdown-view">{dom ? dom : <div>Loading...</div>}</Box>
-  )
+  return <Box className="markdown-view">{dom || <div>Loading...</div>}</Box>
 }
